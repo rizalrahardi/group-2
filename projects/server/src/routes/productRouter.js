62d341a329f2343d38562255248b01065eecb2a5
Rizal Rahardi <rizalrahardi@gmail.com>
2023-08-02 16:23:24 +0700

    fix duplicate export import

--- projects/server/src/routes/productRouter.js
+++ projects/server/src/routes/productRouter.js
@@ -1,42 +1,41 @@
 const router = require("express").Router();
-const {productController} = require('../controllers');
+const { productController } = require('../controllers');
 const { verifyToken } = require("../middlewares/auth");
-const { productController } = require("../controllers");
 const { errorValidate } = require("../middlewares");
 const { multerUpload } = require("../middlewares/multer");
 const { validateProduct } = require("../services");
 
-router.get("/", verifyToken, productController.getProductList )
+router.get("/", verifyToken, productController.getProductList)
 module.exports = router
 router.post(
     "/category",
-	validateProduct.categoryRules,
-	errorValidate,
-	productController.createCategory
+    validateProduct.categoryRules,
+    errorValidate,
+    productController.createCategory
 )
 router.patch(
     "/category/:id",
-	validateProduct.categoryRules,
-	errorValidate,
-	productController.editCategory
+    validateProduct.categoryRules,
+    errorValidate,
+    productController.editCategory
 )
 router.delete(
     "/category/:id",
-	productController.deleteCategory
+    productController.deleteCategory
 )
 router.post(
     "/",
     multerUpload.single("products"),
-	validateProduct.productRules,
-	errorValidate,
-	productController.createProduct
+    validateProduct.productRules,
+    errorValidate,
+    productController.createProduct
 )
 router.patch(
     "/:id",
     multerUpload.single("products"),
-	validateProduct.productRules,
-	errorValidate,
-	productController.editProduct
+    validateProduct.productRules,
+    errorValidate,
+    productController.editProduct
 )
 
 module.exports = router;
